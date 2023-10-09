629e04f8c998a61fd5b6fe1d2c84d684e3c573a4
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-10-08 18:46:01 -0700

    Fixed register() and password hashing.

--- backend/db.py
+++ backend/db.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 db = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -14,27 +15,28 @@ db = mysql.connector.connect(
   database="dubiousdb"
 )
 
-def hash_password(password: str) -> bytes:
-  hashed = hashlib.pbkdf2_hmac("sha256", password.encode(), 110000)
-  return hashed
+def hash_password(password: str):
+  salt = os.urandom(16)
+  hashed = hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), salt, 110000)
+  return hashed, salt
 
-def check_password(hashed: bytes, password: str) -> bool:
-  return hmac.compare_digest(hashed, hashlib.pbkdf2_hmac("sha256", password.encode(), 110000))
+def check_password(hashed: bytes, password: str, salt: bytes) -> bool:
+  return hmac.compare_digest(hashed, hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), salt, 110000))
 
-@app.route('/api/register', methods = ['POST'])
+@app.route('/api/register', methods=['POST'])
 def register():
-  username = request.args.get('username', type = str)
-  password = request.args.get('password', type = str)
-  email = request.args.get('email', type = str)
-  first_name = request.args.get('first_name', type = str)
-  last_name = request.args.get('last_name', type = str)
+  username = request.json['username']
+  password = request.json['password']
+  email = request.json['email']
+  first_name = request.json['first_name']
+  last_name = request.json['last_name']
 
   cursor = db.cursor()
-  hashed_password = hash_password(password)
+  hashed_password, salt = hash_password(password)
 
   try:
-    sql = "SElECT INTO user (userName, passWord, email, firstName, lastName) VALUES (%s, %s, %s, %s, %s)"
-    values = (username, hashed_password.hexdigest(), email, first_name, last_name)
+    sql = "INSERT INTO user (userName, passWord, passSalt, email, firstName, lastName) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = (username, hashed_password, salt, email, first_name, last_name)
     cursor.execute(sql, values)
 
     db.commit()
@@ -42,13 +44,13 @@ def register():
     print(e)
     return "FAILED"
 
-  print(f"{cursor.rowcount} record{"" if cursor.rowcount == 1 else "s"} inserted.")
+  print(f"{cursor.rowcount} record{'' if cursor.rowcount == 1 else 's'} inserted.")
   return "SUCCESS"
 
 @app.route('/api/login', methods=["POST"])
 def login():
-  username = request.args.get('username', type = str)
-  password = request.args.get('password', type = str)
+  username = request.json['username']
+  password = request.json['password']
 
   cursor = db.cursor()
 
@@ -73,4 +75,4 @@ def logout():
   return "Session closed", 401
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
