55d4974c8b9e190df2a84aa3ae0c6bad8ae34207
Alisha A <56981295+alishcodes@users.noreply.github.com>
2023-11-23 10:12:03 -0800

    Add buttons to favorites page.

--- comp440-app/src/app/dashboard/favorites/page.js
+++ comp440-app/src/app/dashboard/favorites/page.js
@@ -1,8 +1,21 @@
+"use client";
+
+import {Stack, Grid, Button} from "@mui/material";
+
 const Favorites = () => {
+
+
     return (
-        <div>
-            Favorites Page.
-        </div>
+        <Stack direction={"column"} justifyContent={"center"} alignItems={"center"} paddingTop={5}>
+            <Grid container spacing={2} maxWidth={400}>
+                <Grid item xs={6}>
+                    <Button variant="outlined">Favorite Items</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant="outlined">Favorite Sellers</Button>
+                </Grid>
+            </Grid>
+        </Stack>    
     )
 }
 
