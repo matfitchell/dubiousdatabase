55a22b23842a00b7c26cf9b6ea5004ef1de51671
Alisha A <56981295+alishcodes@users.noreply.github.com>
2023-11-26 15:56:30 -0800

    Change data format of users.

--- comp440-app/src/app/components/sqlComponents/list7.js
+++ comp440-app/src/app/components/sqlComponents/list7.js
@@ -1,43 +1,33 @@
 "use client";
 
 import {Stack} from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const List7 = () => {
-    const testData = [
-        {
-            username: "Alice"
-        },
-        {
-            username: "John"
-        },
-        {
-            username: "Emma"
-        }
-    ]
-    const [data, setData] = useState(testData);
+    // const testData = ["Alice", "John", "Emma"];
+    const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     fetch("http://localhost:5000/api/seven", {
-    //         method: "GET",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         }
-    //     }).then(response => response.json())
-    //     .then(json => {
-    //         if (json && !json.message) {
-    //             setData(json)
-    //         }
-    //     })
-    //     .catch(err => console.log(err));
-    // }, [])
+    useEffect(() => {
+        fetch("http://localhost:5000/api/seven", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(response => response.json())
+        .then(json => {
+            if (json && !json.message) {
+                setData(json)
+            }
+        })
+        .catch(err => console.log(err));
+    }, [])
 
     return (
         <Stack direction="column" justifyContent="center" alignItems="center" marginBottom={2}>
             <ol>
             {data && data.map((user, index) => 
                 <li key={index}>
-                    <strong>Username:</strong> {" " + user.username}
+                    <strong>Username:</strong> {" " + user}
                 </li>
             )}
             </ol>
